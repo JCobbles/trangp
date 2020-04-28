6999287626101c73fbd570c9d926d8be5618dfae
Jacob Moss <cob.mossy@gmail.com>
2020-04-28 14:23:59 +0100

    add data loader for 3day dros

--- reggae/data_loaders.py
+++ reggae/data_loaders.py
@@ -1,6 +1,25 @@
 import csv
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
+
+def load_3day_dros():
+    with open('data/3day/GSE47999_Normalized_Counts.txt', 'r', 1) as f:
+        contents = f.buffer
+        df = pd.read_table(contents, sep='\t', index_col=0)
+    replicates = 3
+    columns = df.columns[df.columns.str.startswith('20,000')][::replicates]
+    known_target_genes = ['FBgn0011774', 'FBgn0030189', 'FBgn0031713', 'FBgn0032393', 'FBgn0037020', 'FBgn0051864']
+    tf_names = ['FBgn0039044']
+    genes_df = df[df.index.isin(known_target_genes)][columns]
+    tfs_df = df[df.index.isin(tf_names)][columns]
+
+    #Normalise across time points
+    normalised = preprocessing.normalize(np.r_[genes_df.values,tfs_df.values])
+    genes = normalised[:-1]
+    tfs = np.atleast_2d(normalised[-1])
+    return (genes_df, np.float64(genes)), (tfs_df, np.float64(tfs)), np.array([2, 10, 20])
+
 
 def load_barenco_puma():
     mmgmos_processed = True
@@ -42,9 +61,14 @@ def load_barenco_puma():
     Y = np.exp(genes+Y_var/2)
     Y_var = (np.exp(Y_var)-1) * np.exp(2*genes + Y_var)
 
-    scale = np.sqrt(np.var(Y, axis=1));
+    scale = np.sqrt(np.var(Y, axis=1))
     scale_mat = np.c_[[scale for _ in range(7)]].T
     Y = Y / scale_mat
     Y_var = Y_var / (scale_mat * scale_mat)
 
-    return df_genes, genes, genes_se, Y, Y_var
+    m_observed = np.float64(Y[:-1])
+    f_observed = np.float64(np.atleast_2d(Y[-1]))
+    σ2 = np.float64(Y_var[:-1])
+    σ2_f = np.float64(np.atleast_2d(Y_var[-1]))
+
+    return df_genes, genes, genes_se, m_observed, f_observed, Y_var, σ2, σ2_f, np.arange(7)*2           # Observation times
