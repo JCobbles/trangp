fbc2629d83f7fccdbb11b5ff70f00eb2b86f943f
Jacob Moss <cob.mossy@gmail.com>
2020-04-30 15:27:13 +0100

    data same as lawrence

--- reggae/data_loaders.py
+++ reggae/data_loaders.py
@@ -3,6 +3,15 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+class DataHolder(object):
+    def __init__(self, data, noise, time):
+        self.m_obs, self.f_obs = data
+        if noise is not None:
+            self.σ2_m_pre, self.σ2_f_pre = noise
+        self.t = time[0]
+        self.τ = time[1]
+        self.common_indices = time[2]
+
 def load_3day_dros():
     with open('data/3day/GSE47999_Normalized_Counts.txt', 'r', 1) as f:
         contents = f.buffer
@@ -28,7 +37,7 @@ def load_barenco_puma():
             df = pd.read_csv(f, index_col=0)
         with open('data/barencoPUMA_se.csv', 'r') as f:
             dfe = pd.read_csv(f, index_col=0)
-        columns = [f'cARP1-{t}hrs.CEL' for t in np.arange(7)*2]
+        columns = [f'cARP{r}-{t}hrs.CEL' for r in range(1, 4) for t in np.arange(7)*2]
     else:
         with open('data/barenco_processed.tsv', 'r') as f:
             df = pd.read_csv(f, delimiter='\t', index_col=0)
@@ -53,22 +62,32 @@ def load_barenco_puma():
     genes_se.rename(index=index, inplace=True)
 
     # Reorder genes
-    df_genes = genes.reindex(['DDB2', 'SESN1', 'TNFRSF10b', 'p21', 'BIK', 'p53'])
-    genes = df_genes.values
-    genes_se = genes_se.reindex(['DDB2', 'SESN1', 'TNFRSF10b', 'p21', 'BIK', 'p53']).values
+    genes_df = genes.reindex(['DDB2', 'BIK', 'TNFRSF10b', 'p21', 'SESN1', 'p53'])
+    genes_se = genes_se.reindex(['DDB2', 'BIK', 'TNFRSF10b', 'p21', 'SESN1', 'p53'])
 
-    Y_var = genes_se*genes_se
-    Y = np.exp(genes+Y_var/2)
-    Y_var = (np.exp(Y_var)-1) * np.exp(2*genes + Y_var)
+    tfs_df = genes_df.iloc[-1:]
+    genes_df = genes_df.iloc[:-1]
+    genes = genes_df.values
+    tfs = tfs_df.values
 
-    scale = np.sqrt(np.var(Y, axis=1))
-    scale_mat = np.c_[[scale for _ in range(7)]].T
-    Y = Y / scale_mat
-    Y_var = Y_var / (scale_mat * scale_mat)
+    tf_var = genes_se.iloc[-1:].values**2
+    gene_var = genes_se.iloc[:-1].values
+    gene_var = gene_var*gene_var
 
-    m_observed = np.float64(Y[:-1])
-    f_observed = np.float64(np.atleast_2d(Y[-1]))
-    σ2 = np.float64(Y_var[:-1])
-    σ2_f = np.float64(np.atleast_2d(Y_var[-1]))
+    tfs_full = np.exp(tfs + tf_var/2)
+    genes_full = np.exp(genes+gene_var/2)
 
-    return df_genes, genes, genes_se, m_observed, f_observed, Y_var, σ2, σ2_f, np.arange(7)*2           # Observation times
+    tf_var_full = (np.exp(tf_var)-1)*np.exp(2*tfs + tf_var)
+    gene_var_full = (np.exp(gene_var)-1)*np.exp(2*genes + gene_var) # This mistake is in Lawrence et al.
+
+    tf_scale = np.sqrt(np.var(tfs_full[:, :7], ddof=1))
+    tf_scale = np.c_[[tf_scale for _ in range(7*3)]].T
+    tfs = np.float64(tfs_full / tf_scale).reshape(3, 7)
+    tf_var = (tf_var_full / tf_scale**2).reshape(3, 7)
+
+    gene_scale = np.sqrt(np.var(genes_full[:,:7], axis=1, ddof=1))
+    gene_scale = np.c_[[gene_scale for _ in range(7*3)]].T
+    genes = np.float64(genes_full / gene_scale).reshape(5, 3, 7).swapaxes(0, 1)
+    gene_var = np.float64(gene_var_full / gene_scale**2).reshape(5, 3, 7).swapaxes(0, 1)
+
+    return (genes_df, genes), (tfs_df, np.float64(tfs)), gene_var, tf_var, np.arange(7)*2           # Observation times
